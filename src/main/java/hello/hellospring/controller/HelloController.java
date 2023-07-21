ed3b2f891541b0de4e01cdf254bd5d97977c70ee
yunhuiGwak <kwakyh17@naver.com>
2023-07-21 19:47:05 +0900

    HelloController 생성 - model에 data를 담아서 전달 - param을 받아서 전달

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); // data 부분이 아래 hello.html의 data로 치환된다.
+        return "hello"; // resources>templates>hello.html 찾아서 랜더링
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
