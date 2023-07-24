ec702a75a61fe04877aa0befd7d92bb9c55f2714
yunhuiGwak <kwakyh17@naver.com>
2023-07-25 00:02:53 +0900

    회원 리포지터리 인터페이스 생성 - 회원 저장 - id로 회원 조회 - name으로 회원 조회 - 모든 회원 조회

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //회원을 저장
+
+    // id,name으로 조회 시 null일경우 Optional로 감싸서 반환
+    Optional<Member> findById(Long id); // id로 회원조회
+    Optional<Member> findByName(String name); // name으로 조회
+    List<Member> findAll(); // 모든회원 리스트 반환
+}
