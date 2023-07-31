927cd381e29124989181436f3a4b7bcd66e7275d
yunhuiGwak <kwakyh17@naver.com>
2023-07-31 20:41:59 +0900

    Java코드로 직접 스프링 빈 등록(생성자 주입)

--- src/main/java/io/devlabs/intro/repository/MemberRepository.java
+++ src/main/java/io/devlabs/intro/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package io.devlabs.intro.repository;
 
 import io.devlabs.intro.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
