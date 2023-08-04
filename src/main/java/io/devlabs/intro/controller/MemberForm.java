eda64a44d9eb9604caefb315a04b4992c5771523
yunhuiGwak <kwakyh17@naver.com>
2023-08-04 15:09:28 +0900

    회원 등록 컨트롤러 생성 - 웹 등록 화면에서 데이터를 전달 받을 폼 객체

---
+++ src/main/java/io/devlabs/intro/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package io.devlabs.intro.controller;
+
+public class MemberForm {
+    // createMemberForm.html 의 name 과 매칭
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
