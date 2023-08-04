4a50579369b572c1bdb208446d2045e0c9aab17e
yunhuiGwak <kwakyh17@naver.com>
2023-08-04 15:08:16 +0900

    회원 관리 예제 - 회원 등록, 조회 기능 추가

--- src/main/java/io/devlabs/intro/controller/MemberController.java
+++ src/main/java/io/devlabs/intro/controller/MemberController.java
@@ -1,8 +1,14 @@
 package io.devlabs.intro.controller;
 
+import io.devlabs.intro.domain.Member;
 import io.devlabs.intro.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 스프링 컨테이너가 @Controller 애너테이션이 있으면 MemberController 객체를 생성해서 넣어두고 스프링이 관리한다. ->스프링 컨테이너에서 스프링 빈이 관리된다.
 @Controller
@@ -18,5 +24,30 @@ public class MemberController {
     @Autowired // 스프링이 스프링컨테이너에 있는 memberService 가져와서 연결시켜준다. DI(Dependency Injection 의존관계 주입)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member); // 회원가입
+
+        return "redirect:/"; // 홈 화면으로 redirect
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        // key: members에 memberList를 model에 담아서 전달
+        model.addAttribute("members", members);
+        // view template에 전달
+        return "members/memberList";
     }
 }
